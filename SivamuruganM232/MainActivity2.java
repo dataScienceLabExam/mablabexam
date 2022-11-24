d12afa1d13098b33edd3cd3e198fdd84c737b853
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-11-24 16:20:13 +0530

    Create MainActivity2.java

---
+++ SivamuruganM232/MainActivity2.java
@@ -0,0 +1,24 @@
+package com.example.sivamuruganminternallab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        signup = findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
